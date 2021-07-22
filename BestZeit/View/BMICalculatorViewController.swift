783689a569516c018cfc73f4de47c626a6c9c48b
Mousa <>
2021-07-22 19:51:32 +0200

    add BMICalculatorViewController

---
+++ BestZeit/View/BMICalculatorViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BMICalculatorViewController.swift
+//  BestZeit
+//
+//  Created by Mousa on 22.07.21.
+//
+
+import UIKit
+
+class BMICalculatorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
