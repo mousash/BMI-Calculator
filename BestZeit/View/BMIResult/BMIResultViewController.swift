dfda9b7643cfd79c3ae0d240ba46ca0f2f9fe3eb
Mousa Sharifi <>
2021-07-22 21:36:31 +0200

    set BMI result view elements

--- BestZeit/View/BMIResult/BMIResultViewController.swift
+++ BestZeit/View/BMIResult/BMIResultViewController.swift
@@ -11,6 +11,7 @@ class BMIResultViewController: UIViewController {
 
     @IBOutlet weak var headerView: CustomHeader!
     @IBOutlet weak var backButton: CustomButton!
+    @IBOutlet weak var resultLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
